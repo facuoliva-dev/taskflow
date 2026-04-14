9c5bbb5487028b28209f7a301729affcaae6be59
santi-collab-cpu <santimartinez181222@gmail.com>
2026-04-14 15:23:12 -0300

    Taskitem.cs

--- Src/TaskFlow/Models/Taskitem.cs
+++ Src/TaskFlow/Models/Taskitem.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace TaskFlow.Models
 {
-    // Definimos los estados que pide el manual
     public enum TaskStatus
     {
         Pendiente,
@@ -12,20 +11,17 @@ namespace TaskFlow.Models
 
     public class Taskitem
     {
-        // Estos son los campos obligatorios:
         public int ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Responsible { get; set; }
         public TaskStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; } // El ? es porque al principio es nulo
+        public DateTime? UpdatedAt { get; set; }
 
         public Taskitem()
         {
-            // El manual dice que el estado inicial es Pendiente
             Status = TaskStatus.Pendiente;
-            // La fecha de creación debe ser automática
             CreatedAt = DateTime.Now;
         }
     }
